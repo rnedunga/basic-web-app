cc9ee2b942cab0703ddfe6eca8a2cc6c7c9bbd37
rnedunga <rnedunga@andrew.cmu.edu>
2025-10-07 10:02:55 -0400

    Added my second name to query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,7 +12,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "blackbird";
+    return "blackbird2";
   }
 
   return "";
